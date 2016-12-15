ab88f020562c3e90920e8e7ff3d4d39a111331f3
Alexander Dorn <debao84@googlemail.com>
2016-12-15 19:37:22 +0100

    add simple seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+lenovo = Vendor.create(name: 'Lenovo')
+
+lenovo_devices = %w(
+  Thinkpad T500
+  Thinkpad T400
+  Thinkpad X200
+  Thinkpad X220
+  Thinkpad X230
+  Thinkpad T420
+  Thinkpad T430s
+  Thinkpad T520
+  Thinkpad T530
+  Thinkpad T410
+)
+
+lenovo_devices.each do |device|
+  Device.create(vendor: lenovo, needs_rom_dump: true, name: device, config_file: 'random.name')
+end
