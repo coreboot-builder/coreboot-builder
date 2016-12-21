ea0b33b9bd36f39453772696fe5987d9fcc81268
Marcel Schütz <m.schuetz@9elements.com>
2016-12-21 19:29:29 +0100

    fix tabs

--- app/services/options_reader.rb
+++ app/services/options_reader.rb
@@ -3,27 +3,27 @@ class OptionsReader
     ActiveRecord::Base.transaction do
       json_data = JSON.parse(File.read("#{Rails.root}/config/options/options.json"))
 
-			json_data.each do |key, value|
+      json_data.each do |key, value|
 
-				unless option = Option.find_by(label: key)
-					raise "Option #{key} not found"
-				end
-				option.option_values.each(&:destroy)
+        unless option = Option.find_by(label: key)
+          raise "Option #{key} not found"
+        end
+        option.option_values.each(&:destroy)
 
         if option.data_type == "enum"
           value.each do |enum_key, enum_value|
             option.option_values.create(enum_option: enum_key, value: enum_value)
           end
         else
-  				if value.kind_of?(Array)
-  					value.each do |value_entry|
-  						option.option_values.create(value: value_entry)
-  					end
-  				else
-  					option.option_values.create(value: value)
-  				end
+          if value.kind_of?(Array)
+            value.each do |value_entry|
+              option.option_values.create(value: value_entry)
+            end
+          else
+            option.option_values.create(value: value)
+          end
         end
-			end
+      end
     end
   end
 
