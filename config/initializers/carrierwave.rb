e5b946efb12addfcc8cd866b0956af2fbf7cf8f8
Julian Laubstein <contact@julianlaubstein.de>
2016-12-21 19:49:50 +0100

    Switched to carrierwave-ftp

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,10 @@
 CarrierWave.configure do |config|
-  config.storage = :webdav
-  config.webdav_server = ENV['WEBDAV_URL']
-  config.webdav_username = ENV['WEBDAV_USERNAME']
-  config.webdav_password = ENV['WEBDAV_PASSWORD']
+  config.sftp_host = ENV['SFTP_HOST']
+  config.sftp_user = ENV['SFTP_USER']
+  config.sftp_folder = ENV['SFTP_FOLDER']
+  config.sftp_url = ENV['SFTP_URL']
+  config.sftp_options = {
+    :password => ENV['SFTP_PASSWORD'],
+    :port     => ENV['SFTP_PORT']
+  }
 end
