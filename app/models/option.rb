7a56e1d6e7d01932b0421179dbe41dc61e0ceeb1
Marcel Schütz <m.schuetz@9elements.com>
2016-12-23 14:07:12 +0100

    add state to option

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,8 +1,14 @@
 class Option < ActiveRecord::Base
   validates :label, presence: true
   validates :device, presence: true
+  validates :state, presence: true
 
   has_many :option_values
 
   belongs_to :device
+
+  enum state: {
+    active: 0,
+    inactive: 10
+  }
 end
