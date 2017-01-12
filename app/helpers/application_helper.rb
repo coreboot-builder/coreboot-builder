00c06dbf38930bd0d674e2b715ecea48f83eee2a
Marcel Schütz <marcel.schuetz@9elements.com>
2017-01-12 19:03:37 +0100

    add model validations for builds
    add flash messages for errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_messages(opts = {})
+    flash.map do |msg_type, message|
+      content_tag(:div, message, class: "flash-alert flash-alert-#{msg_type}", data: { dismiss: 'alert' }) do
+        concat message
+      end
+    end.join.html_safe
+  end
 end
--- app/models/build.rb
+++ app/models/build.rb
@@ -1,6 +1,9 @@
 class Build < ActiveRecord::Base
   validates :state, presence: true
   validates :email, presence: true
+  validates :device, presence: true, if: :device_chosen_or_beyond?
+  validates :blob_file, presence: true, if: :blob_file_uploaded_or_beyond_and_needs_rom?
+  validates :gpg, presence: true, if: :configured_or_beyond?
 
   belongs_to :device
   has_many :configurations
@@ -10,12 +13,36 @@ class Build < ActiveRecord::Base
   mount_uploader :blob_file, BlobFileUploader
 
   enum state: {
-    unconfigured: 0,
-    configured: 10,
-    pending: 20,
-    succeeded: 30,
-    failed: 40
+    new_build: 0,
+    device_chosen: 10,
+    blob_file_uploaded: 20,
+    options_configured: 30,
+    configured: 40,
+    pending: 50,
+    build_started:60,
+    succeeded: 70,
+    failed: 80
   }
+
+  def device_chosen_or_beyond?
+    %w(device_chosen blob_file_uploaded options_configured configured pending build_started succeeded failed).include?(self.state)
+  end
+
+  def blob_file_uploaded_or_beyond_and_needs_rom?
+    blob_file_uploaded_or_beyond? && device.try(:needs_rom_dump?)
+  end
+
+  def blob_file_uploaded_or_beyond?
+    %w(blob_file_uploaded options_configured configured pending build_started succeeded failed).include?(self.state)
+  end
+
+  def configured_or_beyond?
+    %w(configured pending build_started succeeded failed).include?(self.state)
+  end
+
+  def state_configured_and_needs_blob?
+    state_configured_or_beyond? && device.try(:needs_rom_dump?)
+  end
 
   def create_uuid
     self.uuid = SecureRandom.uuid
