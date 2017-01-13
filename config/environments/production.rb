1ebb549a79e019288a103079bf09db729e58e5f7
Julian Laubstein <contact@julianlaubstein.de>
2017-01-14 00:40:53 +0100

    Adjusted smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,14 +81,13 @@ Rails.application.configure do
 
   config.force_ssl = true
 
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'mail.coreboot.me',
     :authentication => :plain,
-    :address => ENV['MAILGUN_SMTP_SERVER'],
-    :port => ENV['MAILGUN_SMTP_PORT'],
-    :domain => ENV['MAILGUN_DOMAIN'],
-    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-    :password => ENV['MAILGUN_SMTP_PASSWORD'],
   }
+  ActionMailer::Base.delivery_method = :smtp
 end
