33d9e16b9158fc43c70490613a312a732b91ff10
Julian Laubstein <contact@julianlaubstein.de>
2017-01-20 15:09:54 +0100

    Added translation fallback

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,9 @@ module RomOMatic
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
+    config.action_view.raise_on_missing_translations = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
